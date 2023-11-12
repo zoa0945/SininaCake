da43c4dfa1e66d5aefeb96915aeb38ded1adb212
zoa0945 <zoa0945@zoa0945ui-MacBookAir.local>
2023-11-12 17:11:57 +0900

    init: make project file

---
+++ SininaCake/SininaCakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SininaCakeApp.swift
+//  SininaCake
+//
+//  Created by  zoa0945 on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SininaCakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
